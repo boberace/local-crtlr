00d630cf3172a7f620d770f8b6e27bda977d944c
boberace <boberace@github.com>
2024-05-17 08:54:34 -0400

    First commit - added most

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+var cacheName = 'picow-pickup';
+var filesToCache = [
+    './',
+    './index.html',
+    './css/common.css',
+    './css/popup.css',
+    './css/style.css',
+    // './html/effect0.html',
+    // './html/effect1.html',
+    // './html/effect2.html',
+    // './html/effect3.html',
+    './js/main.js' 
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open(cacheName).then(function(cache) {
+        return cache.addAll(filesToCache);
+      })
+    );
+    self.skipWaiting();
+  });
+  
+  /* Serve cached content when offline */
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
